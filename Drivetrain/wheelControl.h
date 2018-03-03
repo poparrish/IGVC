06ed02317296b7e6b75b1a61d56211312e2ae6bb
poparrish <parkerparrish@u.boisestate.edu>
2018-03-03 09:12:01 -0700

    Add files via upload
    planetary PID logic is complete, hub PID needs work for rpm beyond 150 and for reverse direction. Control through the serial monitor with WxBySz. x is wheel select (0-3) B is bearing in degrees (-170-170) S is speed in rpm (50-150). Hub motors currently require power cycle after sending desired speed before they will turn.

---
+++ Drivetrain/wheelControl.h
@@ -0,0 +1,45 @@
+#include "returnedVariables.h"
+class wheelControl {
+  public:
+  float Pgain;
+  float Dgain;
+  int pulsesPerRot;
+  float RPMerror;
+  float DerRPMerror;
+  float lastCycleSpeedCheck;
+  float speedCheckfloat;
+  float lastcycleRPM;
+  float curRPM;
+  float lastDesiredRPM;
+  float RPMincrement;
+  float desiredRPM;
+  int speedCheck;
+  bool forwardBackward;
+  bool backToZero;
+  int cutoff;
+  
+  // inits for planetary motor
+
+  float Perror;
+  float Ierror;
+  float Derror;
+  float lastPerror;
+  float deltaEncPos;
+  float cyclesPerRot;
+  float RPMPlanMot;
+  int lastEncPos;
+  float currentPlanMotPos;
+  float lastPlanMotPos;
+  float deltaPlanMotPos;
+  float derPlanMotPos;
+  float motorSpeed;
+  int intmotorSpeed;
+  float PWheelGain;
+  float DWheelGain;
+  bool PlanMotDir;
+  int Hz;
+
+returnVariables calculate(float a, float b, float c, unsigned long d, int e);
+wheelControl(); 
+};
+
