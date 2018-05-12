3b749196bed125ebc39ea9a3223e2d0def47c650
Chuck <c.burnell@gmail.com>
2018-05-12 16:42:05 -0600

    Created own file for Camera Info
    Created own file for Camera Info

---
+++ Nav-Guidance/camera_info.py
@@ -0,0 +1,33 @@
+
+'''
+A class that is in charge of flattening an image from a camera given
+a known height of the camera
+A known distance to the closest thing the camera can see
+the width that is seen at that closest distance
+a known distance to the "mioddle" of the screen (halfway up what is seen)
+a knowd width that is seen at that middle
+It then does 
+'''
+
+class CameraInfo:
+    def __init__(self, h, d1, w1, d2, w2):
+        self.height = h
+        self.closedist = d1
+        self.closewidth = w1
+        self.middist = d2
+        self.midwidth = w2
+        self.per_inch = 4
+        self.map_height = (self.middist - self.closedist) * self.per_inch
+        self.map_width = self.midwidth * self.per_inch 
+        self.trim = int(((float(self.midwidth - self.closewidth)/2)/float(self.middist - self.closedist))*self.map_height)
+    
+    def convertToFlat(self, image):
+        pts_src = np.float32([[0, 240],[640,240],[0, 480],[640, 480]])
+        left_trim = self.trim
+        #print(left_trim)
+        right_trim = self.map_width - self.trim
+        pts_dst = np.float32([[0, 0],[self.map_width,0],[left_trim, self.map_height],[right_trim, self.map_height]])
+        mtx = cv2.getPerspectiveTransform(pts_src,pts_dst)
+        flat_map = cv2.warpPerspective(image, mtx, (self.map_width, self.map_height))
+        
+        return flat_map
