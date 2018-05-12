86edd2d8e1b1fac0c5aa6ef9e5b000195c26c8b1
Chuck <c.burnell@gmail.com>
2018-05-12 16:41:47 -0600

    Moved CamreaInfo to its own file
    Moved CamreaInfo to its own file

--- Nav-Guidance/cameras.py
+++ Nav-Guidance/cameras.py
@@ -4,7 +4,7 @@ from std_msgs.msg import String
 import cv2
 import numpy as np
 from grip import GripPipelineTest
-
+from camera_info import CameraInfo
 '''
 Configuration stuff
 
@@ -21,39 +21,6 @@ line_thresh_val=[0, 255]
 can_traverse_thresh_hue=[0, 40]
 can_traverse_thresh_sat=[0, 155]
 can_traverse_thresh_val=[20, 255]
-
-'''
-A class that is in charge of flattening an image from a camera given
-a known height of the camera
-A known distance to the closest thing the camera can see
-the width that is seen at that closest distance
-a known distance to the "mioddle" of the screen (halfway up what is seen)
-a knowd width that is seen at that middle
-It then does 
-'''
-
-class CameraInfo:
-    def __init__(self, h, d1, w1, d2, w2):
-        self.height = h
-        self.closedist = d1
-        self.closewidth = w1
-        self.middist = d2
-        self.midwidth = w2
-        self.per_inch = 4
-        self.map_height = (self.middist - self.closedist) * self.per_inch
-        self.map_width = self.midwidth * self.per_inch 
-        self.trim = int(((float(self.midwidth - self.closewidth)/2)/float(self.middist - self.closedist))*self.map_height)
-    
-    def convertToFlat(self, image):
-        pts_src = np.float32([[0, 240],[640,240],[0, 480],[640, 480]])
-        left_trim = self.trim
-        #print(left_trim)
-        right_trim = self.map_width - self.trim
-        pts_dst = np.float32([[0, 0],[self.map_width,0],[left_trim, self.map_height],[right_trim, self.map_height]])
-        mtx = cv2.getPerspectiveTransform(pts_src,pts_dst)
-        flat_map = cv2.warpPerspective(image, mtx, (self.map_width, self.map_height))
-        
-        return flat_map
 
 def processImage(camera_info):
     ret_val, img = cam.read()
@@ -77,7 +44,7 @@ def cameraProcessor():
     while not rospy.is_shutdown():
         local_map, contours  = processImage(camera_info)
         # local_map = np.zeros([3,3,3])
-        local_map_msg = CameraMsg(local_map_val = local_map, contours = contours)
+        local_map_msg = CameraMsg(local_map_val = local_map, contours = contours, camera_info = camera_info)
         local_map_msg_string = local_map_msg.pickleMe()
         # print(len(local_map_msg_string))
         # rospy.loginfo(local_map_msg_string)
